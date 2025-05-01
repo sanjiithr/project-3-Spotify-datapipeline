3e2764a9424057ee88b510748c1e414abed9a121
sanjiithr <151172951+sanjiithr@users.noreply.github.com>
2025-05-01 17:07:12 +0530

    Update spotify_mysql.py

--- spotify_mysql.py
+++ spotify_mysql.py
@@ -7,8 +7,8 @@ import mysql.connector
 
 # Set up Spotify API credentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id='7ae955693f224b679fc0abaad6996b13',  # Replace with your Client ID
-    client_secret='a4b9348c27754ccb92abe396752c15c4'  # Replace with your Client Secret
+    client_id='client-id',  # Replace with your Client ID
+    client_secret='secret-id'  # Replace with your Client Secret
 ))
 
 # MySQL Database Connection
